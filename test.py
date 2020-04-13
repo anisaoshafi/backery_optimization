fc6bece9738f7d567ee53159480fae7d5a3f2976
Anisa Oshafi <anisaoshafi@gmail.com>
2020-04-13 18:17:26 +0200

    Added test

---
+++ test.py
@@ -0,0 +1,16 @@
+from solution import process_order
+
+# packs is a dictionary with existing types of articles.
+# add a new article in this way:
+# packs[code] = {pack_size_1: cost_1, pack_size_2: cost_2, ... pack_size_n: cost_n}
+packs = {'VS5': {3: 6.99, 5: 8.99},
+         'MB11': {2: 9.95, 5: 16.95, 8: 24.95},
+         'CF': {3: 5.95, 5: 9.95, 9: 16.99}
+         }
+
+# Test order
+orders = {'VS5': 10, 'MB11': 14, 'CF': 13}
+
+# process each order and show the optimal packages
+for code, num in orders.items():
+    print(process_order(num, code, packs))
